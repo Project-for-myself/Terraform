c4f20aa995de3be109bb07593a342e524ab350a3
Sviatoslav Kozak <svyatoslav12381@gmail.com>
2024-03-27 10:44:20 +0200

    Added VPC, subnet, sec groups + minor changes

--- main.tf
+++ main.tf
@@ -1,9 +1,54 @@
-resource "aws_instance" "my_vm1" {
+resource "aws_instance" "my_vm" {
   ami           = "ami-023adaba598e661ac" //Ubuntu AMI
   instance_type = "t2.micro"
 
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id              = aws_subnet.subnet1.id
+  key_name               = "DEVOPS"
   tags = {
-    Name     = "My EC2 instance",
+    Name     = "LNPP",
     Provider = "Terraform",
   }
+}
+
+resource "aws_vpc" "frankfurt" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name     = "Frankfurt",
+    Provider = "Terraform",
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.frankfurt.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  type              = "ingress"
+  from_port         = 443
+  protocol          = "tcp"
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  type              = "ingress"
+  from_port         = 443
+  protocol          = "tcp"
+  to_port           = 443
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.frankfurt.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a" // Change this to match your desired availability zone
 }
