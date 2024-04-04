fcb50ba108f312c189c5805f5c85afdb993e30cd
Sviatoslav Kozak <svyatoslav12381@gmail.com>
2024-04-04 20:11:18 +0300

    adding env variables data extraction from state file

--- application/ec2.tf
+++ application/ec2.tf
@@ -1,13 +1,18 @@
-module "network" {
-  source = "../networking"
+data "terraform_remote_state" "networking" {
+  backend = "s3"
+  config = {
+    bucket = "devops.terraform.backend.state"
+    key    = "networking.tf"
+    region = "eu-central-1"
+  }
 }
 
 resource "aws_instance" "my_vm" {
   ami           = "ami-023adaba598e661ac" //Ubuntu AMI
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = module.network.security_group_ids
-  subnet_id              = module.network.subnet_id
+  vpc_security_group_ids = [data.terraform_remote_state.networking.outputs.security_group_ids]
+  subnet_id              = data.terraform_remote_state.networking.outputs.subnet_id
   key_name               = "DEVOPS"
   tags = {
     Name     = "Minecraft",
--- networking/output.tf
+++ networking/output.tf
@@ -3,5 +3,5 @@ output "subnet_id" {
 }
 
 output "security_group_ids" {
-  value = [aws_security_group.allow_ports.id]
+  value = aws_security_group.allow_ports.id
 }
